624d9a4b0993716e687540d9728731fc828b0d40
Nick Frolov <frolovnick0@gmail.com>
2020-09-30 00:22:12 +0300

    init: Test typical-arrays-problems

---
+++ src/index.js
@@ -0,0 +1,27 @@
+
+exports.min = function min(array) {
+    if (!array || !array.length) {
+        return 0;
+    }
+    else {
+    return Math.min(...array);
+    };
+};
+
+exports.max = function max(array) {
+    if (!array || !array.length) {
+        return 0;
+    }
+    else {
+    return Math.max(...array);
+    };
+};
+
+exports.avg = function avg(array) {
+    if (!array || !array.length) {
+        return 0;
+    }
+    else {
+    return array.reduce((acc, curr) => acc + curr, 0) / array.length;
+    };
+};
